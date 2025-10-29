8cd48de27c8526e67c88fc5c6beaa8f7391c850b
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-29 11:06:31 +0100

    Updated

--- extract_data.py
+++ extract_data.py
@@ -1,18 +1,21 @@
 import requests
-import pandas as pd
+from datetime import datetime
 
 def extract_data():
-    
     url = "https://zenquotes.io/api/random"
-
     response = requests.get(url)
-
     data = response.json()
+    quote = f'{data[0]["q"]} - {data[0]["a"]}'
+    return quote
 
-    quote_author= f'{data[0]["q"]} - {data[0]["a"]}'
-
-    print(quote_author)
-
-extract_data()
-
+def save_quote():
+    quote = extract_data()
     
+    filename = f'quote_{datetime.now().strftime("%Y-%m-%d")}.txt'
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(quote)
+    
+    print(f"✅ Quote saved successfully to {filename}")
+
+save_quote()
